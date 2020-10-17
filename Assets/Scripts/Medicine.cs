c9f1e8c345e25f4eee69c051561a7aba99c9fd5f
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-10-17 17:09:05 -0300

    Add medicine item use
    Co-authored-by: Ana Beatriz <abcuelbas@gmail.com>
    Co-authored-by: Guilherme Balog <guilherme.gardino@usp.br>
    Co-authored-by: Gabriel Araújo <ga.briel@usp.br>
    Co-authored-by: Gabriel Michelassi <gabrielmichelassi@usp.br>
    Co-authored-by: Giovana Armani <giovana_armani@usp.br>
    Co-authored-by: Tiago Marino <tmsilva11@gmail.com>

---
+++ Assets/Scripts/Medicine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class Medicine : MonoBehaviour, IPointerClickHandler {
+    public IntVariable Quantity;
+    public TextMeshProUGUI QuantityText;
+
+    private void OnEnable() {
+        QuantityText.text = "x" + Quantity.Value.ToString();
+    }
+
+    void Start() {
+    }
+
+    void Update() {
+    }
+
+    public void UseMedicine() {
+        if (Quantity.Value > 0) {
+            Quantity.DecreaseValue();
+            QuantityText.text = "x" + Quantity.Value.ToString();
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        UseMedicine();
+    }
+}
