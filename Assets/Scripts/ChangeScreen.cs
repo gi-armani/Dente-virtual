bfdf3d9a2fc0d7cf945d2e457658999354c9cd09
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-10-17 16:18:37 -0300

    Add transition between screens
    Co-authored-by: Ana Beatriz <abcuelbas@gmail.com>
    Co-authored-by: Guilherme Balog <guilherme.gardino@usp.br>
    Co-authored-by: Gabriel Araújo <ga.briel@usp.br>
    Co-authored-by: Gabriel Michelassi <gabrielmichelassi@usp.br>
    Co-authored-by: Giovana Armani <giovana_armani@usp.br>
    Co-authored-by: Tiago Marino <tmsilva11@gmail.com>

---
+++ Assets/Scripts/ChangeScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeScreen : MonoBehaviour {
+    public GameObject screenToOpen;
+
+    void Start() {
+        GetComponent<Button>().onClick.AddListener(Change);
+    }
+
+    public void Change() {
+        transform.parent.gameObject.SetActive(false);
+
+        screenToOpen.SetActive(true);
+    }
+}
