a7867d795771b0d9c2cdef994af5074b79c6bdbf
Tiago M. Silva <tmsilva11@gmail.com>
2020-10-19 13:37:30 -0300

    Add Dentinho receives dropped items
    Uses medicine when dropped on dentinho

---
+++ Assets/Scripts/DropReceiver.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropReceiver : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        IDropable droppedObject = null;
+        if(eventData.pointerDrag.TryGetComponent<IDropable>(out droppedObject))
+        {
+            droppedObject.OnDrop();
+        }
+    }
+}
