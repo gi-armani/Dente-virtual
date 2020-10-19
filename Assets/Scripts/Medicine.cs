c348073dacc18b065ecbf7a346e632a133ed7a3d
Tiago M. Silva <tmsilva11@gmail.com>
2020-10-19 13:36:28 -0300

    Add Dropable interface

--- Assets/Scripts/Medicine.cs
+++ Assets/Scripts/Medicine.cs
@@ -5,7 +5,7 @@ using UnityEngine.EventSystems;
 using TMPro;
 using UnityEngine.UI;
 
-public class Medicine : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler {
+public class Medicine : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropable {
     public IntVariable Quantity;
     [SerializeField] private TextMeshProUGUI QuantityText;
     [SerializeField] private Image DragImage;
@@ -16,12 +16,9 @@ public class Medicine : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, 
         QuantityText.text = "x" + Quantity.Value.ToString();
     }
 
-    private void Awake()
-    {
+    private void Awake() {
         _mainCamera = Camera.main;
-    }
-
-    void Update() {
+        DragImage.raycastTarget = false;
     }
 
     public void UseMedicine() {
@@ -32,7 +29,7 @@ public class Medicine : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, 
     }
 
     public void OnPointerClick(PointerEventData eventData) {
-        //UseMedicine();
+        UseMedicine();
     }
 
     public void OnBeginDrag(PointerEventData eventData)
@@ -43,27 +40,16 @@ public class Medicine : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, 
     public void OnDrag(PointerEventData eventData)
     {
         Vector3 position = _mainCamera.ScreenToWorldPoint(eventData.position);
-        position.z = 1;
+        position.z = _mainCamera.transform.position.z + 1;
         DragImage.transform.position = position;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        // Verifica se está emcima do dentinho
-        Ray screenPointRay = _mainCamera.ScreenPointToRay(eventData.position);
-        RaycastHit hitInfo = default;
-        Debug.Log(screenPointRay + "origin: " + screenPointRay.origin);
-        screenPointRay.origin = new Vector3(screenPointRay.origin.x, screenPointRay.origin.y, transform.position.z);
-        //Se sim, usa o remedio
-        if(Physics.Raycast(screenPointRay, out hitInfo))
-        {
-            UseMedicine();
-        }
-        Debug.Log(hitInfo.point);
-
-
         // "devolve" a imagem
         DragImage.transform.position = transform.position;
         DragImage.gameObject.SetActive(false);
     }
+
+    public void OnDrop() => UseMedicine();
 }
