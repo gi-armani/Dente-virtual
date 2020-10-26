f201b93d1fc29fb1aeabc5bbce66b00d0b5fbc1a
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-10-26 17:20:06 -0300

    Add main bar and its update mechanism
    Co-authored-by: Gabriel Michelassi <gabrielmichelassi@usp.br>
    Co-authored-by: Guilherme Balog <guilherme.gardino@usp.br>

---
+++ Assets/Scripts/MainBar.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainBar : MonoBehaviour
+{
+    public BarValues[] BarValuesArray;
+    GameObject fillBar;
+
+    void Awake()
+    {
+        fillBar = GameObject.FindWithTag("FillBar");
+    }
+
+    public void UpdateFill()
+    {
+        if (BarValuesArray.Length <= 0) return;
+
+        Image img = fillBar.GetComponent<Image>();
+        float sum = 0;
+
+        foreach (BarValues barValues in BarValuesArray)
+        {
+            sum += barValues.FillPercentage;
+        }
+
+        img.fillAmount = sum / BarValuesArray.Length;
+    }
+
+    private void OnEnable()
+    {
+        UpdateFill();
+        foreach (BarValues barValues in BarValuesArray)
+        {
+            barValues.OnFillChange += UpdateFill;
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (BarValues barValues in BarValuesArray)
+        {
+            barValues.OnFillChange -= UpdateFill;
+        }
+    }
+}
