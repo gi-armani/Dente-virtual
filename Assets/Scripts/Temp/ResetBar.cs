279c95e52326acc339a6314244531acd8223cc6a
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-10-26 17:29:48 -0300

    Add temporary ResetBar button to improve testing
    Co-authored-by: Gabriel Michelassi <gabrielmichelassi@usp.br>
    Co-authored-by: Guilherme Balog <guilherme.gardino@usp.br>

---
+++ Assets/Scripts/Temp/ResetBar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetBar : MonoBehaviour
+{
+    public BarValues[] BarValuesArray;
+
+    public void ResetBars()
+    {
+        foreach (BarValues barValues in BarValuesArray)
+        {
+            barValues.AddFillPercentage(1);
+        }
+    }
+
+}
