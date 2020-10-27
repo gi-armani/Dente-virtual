a746575ccedba27e9739b543c521e8c84be08d1f
Tiago M. Silva <tmsilva11@gmail.com>
2020-10-27 18:53:51 -0300

    Fix Test
    Problem was in WaitForEndOfFrame

--- Assets/Editor/PlayModeTests/DecaySystemTests.cs
+++ Assets/Editor/PlayModeTests/DecaySystemTests.cs
@@ -9,7 +9,7 @@ namespace Tests
 {
     public class DecaySystemTests
     {
-        [UnityTest] [Timeout(2000)]
+        [UnityTest]
         public IEnumerator decay_system_decrease_fill_quantity_over_time()
         {
             // Prepare Scene
@@ -24,7 +24,6 @@ namespace Tests
             system.BarValuesArray = new BarValues[1] { barValue };
 
             // Act --> Acts on Start
-            //yield return new WaitForEndOfFrame();
             yield return null;
             yield return new WaitForSeconds(barValue.DecayCooldown);
 
@@ -34,35 +33,6 @@ namespace Tests
             // Clean
             GameObject.DestroyImmediate(DecaySystemObject);
             ScriptableObject.DestroyImmediate(barValue);
-        }
-
-        [UnityTest] [Timeout(2000)]
-        public IEnumerator decay_system_decrease_fill_quantity_over_time_with_unload()
-        {
-            // Prepare Scene
-            GameObject DecaySystemObject = new GameObject();
-            DecaySystem system = DecaySystemObject.AddComponent<DecaySystem>();
-
-            var barValue = ScriptableObject.CreateInstance<BarValues>();
-            barValue.FillPercentage = 1;
-            barValue.DecayCooldown = .1f;
-            barValue.DecayQuantity = .1f;
-
-            system.BarValuesArray = new BarValues[1] { barValue };
-
-            // Act --> Acts on Start
-            //yield return new WaitForEndOfFrame();
-            yield return null;
-            yield return new WaitForSeconds(barValue.DecayCooldown);
-
-            // Assert
-            Assert.AreEqual(1f - barValue.DecayQuantity, barValue.FillPercentage, .00001f, $"Bar value fill is at: {barValue.FillPercentage}");
-
-            // Clean
-            GameObject.DestroyImmediate(DecaySystemObject);
-            ScriptableObject.DestroyImmediate(barValue);
-
-            SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
