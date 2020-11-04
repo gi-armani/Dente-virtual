64fc0c18411e8e1b50c3ab99025a824c22ff2bbd
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-11-04 19:09:22 -0300

    Fix bracket style

--- Assets/Scripts/Thermometer.cs
+++ Assets/Scripts/Thermometer.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Thermometer : MonoBehaviour {
+public class Thermometer : MonoBehaviour
+{
     [SerializeField] private BarValues barValue;
     [SerializeField] private float lowerLimit = 0.4f;
 
     Color opaque = new Color(1, 1, 1, 1);
     Color transparent = new Color(1, 1, 1, 0);
 
-    void Update() {
-        if (barValue.FillPercentage < lowerLimit) {
+    void Update()
+    {
+        if (barValue.FillPercentage < lowerLimit)
+        {
             GetComponent<Image>().color = opaque;
         }
-        else {
+        else
+        {
             GetComponent<Image>().color = transparent;
         }
     }
