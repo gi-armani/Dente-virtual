89c4b60fb497244631e3b716feba208fcdacb36f
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-11-04 19:02:48 -0300

    Add thermometer symbol when dentinho is sick
    Co-authored-by: Gabriel Araújo <ga.briel@usp.br>

---
+++ Assets/Scripts/Thermometer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Thermometer : MonoBehaviour {
+    [SerializeField] private BarValues barValue;
+    [SerializeField] private float lowerLimit = 0.4f;
+
+    Color opaque = new Color(1, 1, 1, 1);
+    Color transparent = new Color(1, 1, 1, 0);
+
+    void Update() {
+        if (barValue.FillPercentage < lowerLimit) {
+            GetComponent<Image>().color = opaque;
+        }
+        else {
+            GetComponent<Image>().color = transparent;
+        }
+    }
+}
