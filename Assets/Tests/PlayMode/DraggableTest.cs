35667ccefd9bc75ac05b873a6a0ccc4c5471c9e5
Tiago M. Silva <tmsilva11@gmail.com>
2020-11-05 12:03:37 -0300

    Prepare Test Scene for Draggable Tests
    Co-authored-by: Guilherme Balog <guilherme.gardino@usp.br>

---
+++ Assets/Tests/PlayMode/DraggableTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+
+namespace Tests
+{
+    public class DraggableTest
+    {
+        // Funções pra lidar com o load da cena de teste
+        bool sceneLoaded;
+        public void LoadTestScene()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            SceneManager.LoadScene("TestDrag", LoadSceneMode.Single);
+        }
+
+        void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            sceneLoaded = true;
+        }
+    }
+}
