a98e2a31f8a8b39f6850f60cbe794da286fba84a
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-11-05 18:03:02 -0300

    Add shower panel icons and the appearance of movement images
    Co-authored-by: Ana Beatriz <abcuelbas@gmail.com>
    Co-authored-by: Guilherme Balog <guilherme.gardino@usp.br>
    Co-authored-by: Gabriel Araújo <ga.briel@usp.br>
    Co-authored-by: Gabriel Michelassi <gabrielmichelassi@usp.br>
    Co-authored-by: Giovana Armani <giovana_armani@usp.br>
    Co-authored-by: Tiago Marino <tmsilva11@gmail.com>

---
+++ Assets/Scripts/BrushMovementHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrushMovementHandler : MonoBehaviour
+{
+    static GameObject lastActive;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public static void DisplayImage(GameObject image)
+    {
+        lastActive?.SetActive(false);
+        image?.SetActive(true);
+
+        lastActive = image;
+    }
+}
---
+++ Assets/Scripts/ShowerItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowerItem : MonoBehaviour
+{
+    [SerializeField] private GameObject displayImage;
+
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(ShowMove);
+    }
+
+    void ShowMove()
+    {
+        BrushMovementHandler.DisplayImage(displayImage);
+    }
+}
