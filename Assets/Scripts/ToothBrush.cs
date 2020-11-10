dd65d2a5d0e95200d2aae3866a48c52eda59278f
Tiago M. Silva <tmsilva11@gmail.com>
2020-11-10 16:14:01 -0300

    Start ToothBrush Drag
    Co-authored-by: Ana Beatriz <abcuelbas@gmail.com>
    Co-authored-by: Guilherme Balog <guilherme.gardino@usp.br>
    Co-authored-by: Eduardo Yukio <eduardoyrodrigues@gmail.com>
    Co-authored-by: Gabriel Araújo <ga.briel@usp.br>
    Co-authored-by: Gabriel Michelassi <gabrielmichelassi@usp.br>
    Co-authored-by: Giovana Armani <giovana_armani@usp.br>

---
+++ Assets/Scripts/ToothBrush.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ToothBrush : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField] private Camera mainCamera = null;
+    [SerializeField] private Image toothBrush = null;
+
+    private Vector3 _initialPosition = Vector3.zero;
+
+    private void Start()
+    {
+        _initialPosition = toothBrush.transform.position;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin Drag");
+        toothBrush.enabled = true;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 position = mainCamera.ScreenToWorldPoint(eventData.position);
+        position.z = mainCamera.transform.position.z + 1;
+        toothBrush.transform.position = position;
+        Debug.Log("Dragging");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        toothBrush.enabled = false;
+        toothBrush.transform.position = transform.position;
+    }
+}
