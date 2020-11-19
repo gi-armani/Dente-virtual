0f85f729b7584381f5f9e2a6d9540ac34cfe082f
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-11-19 17:46:04 -0300

    Remove space excesses

--- Assets/Scripts/PersistentScriptableObject.cs
+++ Assets/Scripts/PersistentScriptableObject.cs
@@ -15,38 +15,38 @@ public abstract class PersistentScriptableObject : ScriptableObject
 
     public void Load(string path)
     {
-        if(File.Exists(path))
+        if (File.Exists(path))
         {
             var json = File.ReadAllText(path);
             JsonUtility.FromJsonOverwrite(json, this);
-        } 
-        else 
+        }
+        else
         {
             Save(path);
         }
     }
 
-    private string GetPath(){
+    private string GetPath()
+    {
         var directoryPath = Path.Combine(Application.persistentDataPath, this.GetType().Name);
 
-        if(!Directory.Exists(directoryPath)){
+        if (!Directory.Exists(directoryPath))
+        {
             Directory.CreateDirectory(directoryPath);
         }
 
-        return Path.Combine(directoryPath, name+".json");
+        return Path.Combine(directoryPath, name + ".json");
     }
 
-    private void OnEnable() 
+    private void OnEnable()
     {
         string path = GetPath();
-        // Debug.Log($"Loaded from {path}");
         Load(path);
     }
 
-    private void OnDestroy() {
+    private void OnDestroy()
+    {
         string path = GetPath();
-        Debug.Log                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       ($"Saving on {path}");
         Save(path);
     }
-
 }
