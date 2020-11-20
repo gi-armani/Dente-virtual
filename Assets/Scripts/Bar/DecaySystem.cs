5a7797671794995f23218ecf81dbb24b84b2ac40
Tiago M. Silva <tmsilva11@gmail.com>
2020-11-20 16:57:05 -0300

    Save and Load Closed Time to decay offline
    - with TDD
    Co-authored-by: Guilherme Balog <guilherme.gardino@usp.br>

--- Assets/Scripts/Bar/DecaySystem.cs
+++ Assets/Scripts/Bar/DecaySystem.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DecaySystem : MonoBehaviour
 {
-    public BarValues[] BarValuesArray;
+    public BarValues[] BarValuesArray = new BarValues[0];
     private void Start()
     {
         BarSelect();
@@ -25,4 +26,39 @@ public class DecaySystem : MonoBehaviour
         }
     }
 
+    public static readonly string closeDateKey = "LastCloseDateTime";
+
+    public bool Load(DateTime time)
+    {
+        string closedTimeStr = PlayerPrefs.GetString(closeDateKey, string.Empty/*DateTime.Now.ToUniversalTime().ToString()*/);
+
+        if (string.IsNullOrEmpty(closedTimeStr))
+        {
+            Debug.LogWarning("Couldn't load closed time");
+            return false;
+        }
+
+        if(DateTime.TryParse(closedTimeStr, out DateTime closedDateTime))
+        {
+            time = time.ToUniversalTime();
+
+            double passedTime = (time - closedDateTime).TotalSeconds;
+            foreach(BarValues bar in BarValuesArray)
+            {
+                int decayTimes = Mathf.FloorToInt((float) passedTime / bar.DecayCooldown);
+                bar.AddFillPercentage(-bar.DecayQuantity * decayTimes);
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void Save(DateTime time)
+    {
+        PlayerPrefs.SetString(closeDateKey, time.ToUniversalTime().ToString());
+        PlayerPrefs.Save();
+    }
 }
