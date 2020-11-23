ed208483e9870395908cef53ed7ea55e2ebd3aa7
anabcuelbas <abcuelbas@gmail.com>
2020-11-23 20:07:07 -0300

    Start buying mechanism
    Co-authored-by: Guilherme Balog <guilherme.gardino@usp.br>
    Co-authored-by: Gabriel Araújo <ga.briel@usp.br>

--- Assets/Scripts/Shop/ShopScreenBuyButton.cs
+++ Assets/Scripts/Shop/ShopScreenBuyButton.cs
@@ -5,18 +5,23 @@ using UnityEngine.UI;
 
 public class ShopScreenBuyButton : MonoBehaviour
 {
+    [SerializeField] private Resources resources = default;
+    [SerializeField] private Inventory inventory = default;
     public GameObject caller;
     void Start()
     {
         GetComponent<Button>().onClick.AddListener(DisableCheck);
+        GetComponent<Button>().onClick.AddListener(BuyItem);
+    }
+
+    public void BuyItem()
+    {
+        var itemName = caller.transform.parent.name;
+        inventory.AddValue(itemName, 1);
     }
 
     public void DisableCheck()
     {
         caller.GetComponent<Image>().color = Color.clear;
-    }
-    void Update()
-    {
-        
     }
 }
