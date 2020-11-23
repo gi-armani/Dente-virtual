6ff0f2169ad679250f4cca9046c3196d71124da2
anabcuelbas <abcuelbas@gmail.com>
2020-11-23 19:42:11 -0300

    Add check image to shop item
    Co-authored-by: Guilherme Balog <guilherme.gardino@usp.br>
    Co-authored-by: Eduardo Yukio <eduardoyrodrigues@gmail.com>
    Co-authored-by: Gabriel Araújo <ga.briel@usp.br>

---
+++ Assets/Scripts/Shop/ShopScreenBuyButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopScreenBuyButton : MonoBehaviour
+{
+    public GameObject caller;
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(DisableCheck);
+    }
+
+    public void DisableCheck()
+    {
+        caller.GetComponent<Image>().color = Color.clear;
+    }
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Shop/ShopScreenItem.cs
+++ Assets/Scripts/Shop/ShopScreenItem.cs
@@ -6,14 +6,26 @@ using UnityEngine.UI;
 public class ShopScreenItem : MonoBehaviour
 {
     [SerializeField] private GameObject buyButton;
+    public GameObject checkImage;
+    private Color opaque = new Color(0,1, 21 / 225, 1);
 
     void Start()
     {
+        checkImage.GetComponent<Image>().color = Color.clear;
         GetComponent<Button>().onClick.AddListener(ShowButton);
     }
 
     void ShowButton()
     {
+        var caller = buyButton.GetComponent<ShopScreenBuyButton>().caller;
+
+        if (caller != null)
+        {
+            buyButton.GetComponent<ShopScreenBuyButton>().DisableCheck();
+        }
+
         buyButton.SetActive(true);
+        checkImage.GetComponent<Image>().color = opaque;
+        buyButton.GetComponent<ShopScreenBuyButton>().caller = checkImage;
     }
 }
