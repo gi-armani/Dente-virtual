9aa2f9ecfd17bb643a51cfb9ec2dc70901f0b72a
gi-armani <giovana_armani@usp.br>
2020-11-27 19:44:33 -0300

    Create script to change clothes
    Co-authored-by: Eduardo Yukio <eduardoyrodrigues@gmail.com>

---
+++ Assets/Scripts/Clothes/ClotheItems.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClotheItems : MonoBehaviour
+{
+    public GameObject Dentinho;
+    public enum ClothesType
+    {
+        ShortHair,
+        LongHair,
+        Cape,
+        Armor,
+    };
+    public ClothesType type;
+
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(ChangeClothes);
+    }
+
+    public void ChangeClothes()
+    {
+        Dentinho.transform.Find("Cape").gameObject.SetActive(false);
+        Dentinho.transform.Find("Armor").gameObject.SetActive(false);
+        Dentinho.transform.Find("ShortHair").gameObject.SetActive(false);
+        Dentinho.transform.Find("LongHair").gameObject.SetActive(false);
+
+        if (type == ClothesType.Cape) Dentinho.transform.Find("Cape").gameObject.SetActive(true);
+        if (type == ClothesType.Armor) Dentinho.transform.Find("Armor").gameObject.SetActive(true);
+        if (type == ClothesType.ShortHair) Dentinho.transform.Find("ShortHair").gameObject.SetActive(true);
+        if (type == ClothesType.LongHair) Dentinho.transform.Find("LongHair").gameObject.SetActive(true);
+
+    }
+}
