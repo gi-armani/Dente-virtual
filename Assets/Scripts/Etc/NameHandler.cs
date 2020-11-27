8b8b149270c1c0f8bf85baeff6c832a484544423
Gabriel <gabrielmichelassi@usp.br>
2020-11-27 15:33:19 -0300

    Create functions to save Dentinho's name
    Co-authored-by: Tiago Marino <tmsilva11@gmail.com>

--- Assets/Scripts/Etc/NameHandler.cs
+++ Assets/Scripts/Etc/NameHandler.cs
@@ -2,21 +2,37 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.IO;
 
 public class NameHandler : MonoBehaviour
 {
     [SerializeField] private TMP_InputField inputField;
     [SerializeField] private Button button;
+    private const string path = "DentinhoNameFile";
 
     private void Awake()
     {
         inputField.onTouchScreenKeyboardStatusChanged.AddListener(ShowButton);
         inputField.onSelect.AddListener(arg0 => Debug.Log("testeee"));
+        button.onClick.AddListener(SaveName);
     }
 
     private void ShowButton(TouchScreenKeyboard.Status tsk)
     {
         if(tsk == TouchScreenKeyboard.Status.Done)
             button.gameObject.SetActive(true);
+    }
+
+    private void SaveName()
+    {
+        if (!String.IsNullOrEmpty(inputField.text))
+        {
+            File.WriteAllText(GetPath(), inputField.text);
+        }
+    }
+    
+    private string GetPath()
+    {
+        return Path.Combine(Application.persistentDataPath, path + ".txt");
     }
 }
