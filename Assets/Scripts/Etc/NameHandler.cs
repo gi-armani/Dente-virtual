3d488b4fd21080aa7bf45020c1e04af2af8546c2
Gabriel <gabrielmichelassi@usp.br>
2020-11-27 15:25:12 -0300

    Create script to handle name of the Dente
    Co-authored-by: Tiago Marino <tmsilva11@gmail.com>

---
+++ Assets/Scripts/Etc/NameHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NameHandler : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField inputField;
+    [SerializeField] private Button button;
+
+    private void Awake()
+    {
+        inputField.onTouchScreenKeyboardStatusChanged.AddListener(ShowButton);
+        inputField.onSelect.AddListener(arg0 => Debug.Log("testeee"));
+    }
+
+    private void ShowButton(TouchScreenKeyboard.Status tsk)
+    {
+        if(tsk == TouchScreenKeyboard.Status.Done)
+            button.gameObject.SetActive(true);
+    }
+}
