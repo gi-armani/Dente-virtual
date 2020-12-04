b9dd2e0f2fff3e2bde1957bf8acf4c32b476587f
Tiago M. Silva <tmsilva11@gmail.com>
2020-12-04 16:27:43 -0300

    Disable feedbacktext when changing screen

--- Assets/Scripts/Etc/FeedbackText.cs
+++ Assets/Scripts/Etc/FeedbackText.cs
@@ -9,20 +9,38 @@ public class FeedbackText : MonoBehaviour
     [SerializeField] private TextMeshProUGUI feedbackText = default;
     [SerializeField] private float timeToHide = 1f;
 
+    /// <summary>
+    /// Show feedback text string on UI
+    /// </summary>
+    /// <param name="feedback"></param>
     public void ShowFeedBackText(string feedback)
     {
         CancelInvoke(nameof(HideFeedBackText));
         feedbackText.SetText(feedback);
     }
 
+    /// <summary>
+    /// Hide the feedback message text
+    /// </summary>
     public void HideFeedBackText()
     {
         feedbackText.SetText(String.Empty);
     }
 
+    /// <summary>
+    /// Show feedback text string on UI and hide it after some time
+    /// </summary>
+    /// <param name="feedback"></param>
     public void ShowTextWithTime(string feedback)
     {
         ShowFeedBackText(feedback);
         Invoke(nameof(HideFeedBackText), timeToHide);
+    }
+
+    // Hide text when changing screens
+    private void OnDisable()
+    {
+        CancelInvoke(nameof(HideFeedBackText));
+        HideFeedBackText();
     }
 }
